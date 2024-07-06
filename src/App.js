53e12077ec743bad061c42bf963a6a9fda742b27
Bavanuja <bbavanuja@gmail.com>
2024-07-06 20:19:05 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-
+function Heading(){
+  return(
+    <h2>This is my heading</h2>
+  );
+}
+function title(){
+  return(
+    
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <p>loosu arushan</p>
+   <Heading/>
+   </div>
+  
+   
   );
 }
 
+
 export default App;
+
+
