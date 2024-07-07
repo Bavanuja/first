f33fb8e8e45508d0057c715ee491e5457c4f29d6
Bavanuja <bbavanuja@gmail.com>
2024-07-07 19:44:51 +0530

    import components

--- src/Components/Intro2.js
+++ src/Components/Intro2.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function Intro2() {
     return (
         <div className="blog-post-intro">
--- src/Components/Promo.js
+++ src/Components/Promo.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function Promo() {
     return (
         <div className="promo-section">
